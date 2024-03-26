153bac89fc9b1b4199af89f383a78ad5cbdaed9d
csmin0212 <162344690+csmin0212@users.noreply.github.com>
2024-03-26 07:08:38 +0000

    end of file!

--- fun.impl.c
+++ fun.impl.c
@@ -13,14 +13,8 @@ bool is_exit(int option) { return option = EXIT; }
 
 int add(int a, int b) { return a + b; }
 
-int subtract(int a, int b) {
-  // Your code here
-}
+int subtract(int a, int b) { return a - b; }
 
-int multiply(int a, int b) {
-  // Your code here
-}
+int multiply(int a, int b) { return a * b; }
 
-int divide(int a, int b) {
-  // Your code here
-}
+int divide(int a, int b) { return a / b; }
